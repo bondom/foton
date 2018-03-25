2fd2fba5e1872ac7ee585b1717926095a4c5f970
Yurii Fediv <alpachino48@gmail.com>
2018-03-25 12:08:45 +0300

    Replaced text on main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,18 @@
-const API_URL = 'http://77.47.131.170/api/documents';
+const API_URL = "http://77.47.131.170/api/documents";
 
-const mainPage = `Нужна
-  1. Главная страничка<br></br>
-  2. Страничка контакты<br></br>
-  3. Страничка документы<br></br>
-  <br></br>
-  под разрешение 1024 х 768
-  <br></br>
-  главная страничка под стиль логотипа (ранее я придумывал что-то вроде симбиоза украинского орнамента и дорожек печатной платы) эскизы были только в бумаге, сейчас не нашел.
-  цвета светлые,
-  фон ???. переходы по страничкам???? - шрифты??? размер шрифта ??? предложи сам
-  <br></br>
-  Контакты понятно, они у тебя есть.
-  так чтобы было удобно копировать.
-
-  Страничка документы.
-  Там будут скан-копии документов.
-
-  Нужно удобство добавления туда новых, удаления старых (несколько раз в год)
-  удобство скачивания, мини изображения не нужны просто гипер ссылка (устав, платник ПДВ, всего около 6-7 документов)
-
-  будет лежать на www.foton.kiev.ua.`;
+const mainPage = `
+<p><b>ТОВ "НВП "Фотон"</b> надає повний спектр послуг з розробки, 
+виробництва, продажу, монтажу, ремонту і технічного обслуговування 
+радіоелектронної апаратури, засобів зв'язку, систем безпеки, 
+медичного обладнання, відео- і телеапаратури, комп'ютерної техніки, 
+вимірювальних приладів та інших спеціальних засобів і засобів загального призначення.</p> 
+<p><b>ТОВ "НВП "Фотон"</b> має понад 20 висококваліфікованих фахівців, які мають великий досвід у науковій, 
+інженерній та виробничій діяльності з радіотехніці. 
+Їх наукові дослідження, підтверджені конкретними завершеними проектами, 
+які регулярно публікуються в таких спеціалізованих виданнях, як <b>"Правове, 
+нормативне та метрологічне забезпечення системи захисту інформації в Україні"</b>, 
+<b>"Фотон-експрес"</b> та інших. Компанія має свої виробничі потужності, оснащені сучасним обладнанням, 
+що дозволяє вирішувати технічні проблеми будь-якої складності.</p>`;
 
 const contactsPage = `
   <h4>ТОВ “Науково-виробниче підприємство “Фотон”<br></br></h4>
@@ -44,8 +36,11 @@ const content = {
 };
 
 window.onload = function() {
-  console.log('Loaded');
-  const pathname = window.location.pathname.substring(1, window.location.pathname.length);
+  console.log("Loaded");
+  const pathname = window.location.pathname.substring(
+    1,
+    window.location.pathname.length
+  );
   handlePageLinkClick(pathname);
 };
 
@@ -55,62 +50,67 @@ function handlePageLinkClick(name) {
 }
 
 function loadData(name) {
-  if(name === 'documents') {
-  getDocumentsAndInsertIntoDom();
+  if (name === "documents") {
+    getDocumentsAndInsertIntoDom();
   }
 }
 
 function replaceContentAndChangeStyles(name, callback) {
-  const mainContent = document.getElementsByClassName('main-content')[0];
-    mainContent.style="opacity:0";
-    setTimeout(() => {
+  const mainContent = document.getElementsByClassName("main-content")[0];
+  mainContent.style = "opacity:0";
+  setTimeout(() => {
     mainContent.innerHTML = content[name] || content.main;
-    mainContent.style="opacity:1";
+    mainContent.style = "opacity:1";
     callback();
   }, 300);
 
-  const activeLink = document.getElementsByClassName('nav-item-chosen')[0];
-  activeLink.className = activeLink.className.split(' ').filter(cn => cn !== 'nav-item-chosen').join(' ');
+  const activeLink = document.getElementsByClassName("nav-item-chosen")[0];
+  activeLink.className = activeLink.className
+    .split(" ")
+    .filter(cn => cn !== "nav-item-chosen")
+    .join(" ");
 
-  const newActiveLink = document.getElementsByClassName(name)[0] || document.getElementsByClassName('main')[0];
-  newActiveLink.className+=' nav-item-chosen';
+  const newActiveLink =
+    document.getElementsByClassName(name)[0] ||
+    document.getElementsByClassName("main")[0];
+  newActiveLink.className += " nav-item-chosen";
 }
 
 function getDocumentsAndInsertIntoDom(withDeleteBtn = false) {
-  const documentsElement = document.getElementsByClassName('documents-list')[0];
-  documentsElement.innerHTML="";
+  const documentsElement = document.getElementsByClassName("documents-list")[0];
+  documentsElement.innerHTML = "";
   fetch(API_URL, {
-    method: 'GET'
+    method: "GET"
   })
-  .then(array => array.json())
-  .then(array => {
-    array.forEach(obj => {
-      const dataUrl = obj.document.data; // contains array of integers
-      
-      const documentItemElement = document.createElement('div');
-      documentItemElement.appendChild(createLink(obj.document.name, dataUrl));
-      if (withDeleteBtn) {
-        documentItemElement.appendChild(createDeleteButton(obj._id));
-      }
-      documentsElement.appendChild(documentItemElement);
-    });
-  })
-  .catch(err => console.log(err));
-};
+    .then(array => array.json())
+    .then(array => {
+      array.forEach(obj => {
+        const dataUrl = obj.document.data; // contains array of integers
+
+        const documentItemElement = document.createElement("div");
+        documentItemElement.appendChild(createLink(obj.document.name, dataUrl));
+        if (withDeleteBtn) {
+          documentItemElement.appendChild(createDeleteButton(obj._id));
+        }
+        documentsElement.appendChild(documentItemElement);
+      });
+    })
+    .catch(err => console.log(err));
+}
 
 function deleteDocument(documentId) {
-  fetch(API_URL + '/' + documentId, {
-    method: 'DELETE'
+  fetch(API_URL + "/" + documentId, {
+    method: "DELETE"
   })
-  .then(doc => {
-    console.log('Deleted');
-    getDocumentsAndInsertIntoDom(true);
-  })
-  .catch(err => console.log(err));
+    .then(doc => {
+      console.log("Deleted");
+      getDocumentsAndInsertIntoDom(true);
+    })
+    .catch(err => console.log(err));
 }
 
 function createLink(name, dataUrl) {
-  const a = document.createElement('a');
+  const a = document.createElement("a");
   const linkTextNode = document.createTextNode(name);
   a.appendChild(linkTextNode);
   a.title = `Download ${name}`;
@@ -120,8 +120,8 @@ function createLink(name, dataUrl) {
 }
 
 function createDeleteButton(documentId) {
-  const button = document.createElement('button');
-  const buttonTextNode = document.createTextNode('Delete');
+  const button = document.createElement("button");
+  const buttonTextNode = document.createTextNode("Delete");
   button.appendChild(buttonTextNode);
   button.onclick = () => deleteDocument(documentId);
   return button;
